f757ee656521c93040026af06f0ec3e0a3b98caa
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2021-07-26 18:27:46 +0200

    fix compile_tf_graph.py

--- returnn/tf/layers/rec.py
+++ returnn/tf/layers/rec.py
@@ -990,6 +990,8 @@ class _SubnetworkRecCell(object):
     self.parent_rec_layer = None  # type: typing.Optional[RecLayer]
     self.parent_net = parent_net
     self.net_dict = safe_deep_copy(net_dict)
+    self.rec_layer_name = rec_layer_name
+    self.parent_get_layer = parent_get_layer
     from returnn.tf.network import TFNetwork, ExternData, LossHolder
     self.net = TFNetwork(
       name="%s/%s(rec-subnet)" % (parent_net.name, rec_layer_name),
@@ -1039,6 +1041,8 @@ class _SubnetworkRecCell(object):
 
   def set_parent_layer(self, parent_rec_layer):
     """
+    Only called from RecLayer._get_cell to define itself as parent
+
     :param RecLayer parent_rec_layer:
     """
     assert parent_rec_layer.network is self.parent_net
