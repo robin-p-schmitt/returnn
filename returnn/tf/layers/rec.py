a0a937d26ab08824dc13dda5dfd540d094f3e1cb
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2021-08-28 16:42:53 +0200

    fixes for tf_compile_job and tests

--- returnn/tf/layers/rec.py
+++ returnn/tf/layers/rec.py
@@ -990,8 +990,6 @@ class _SubnetworkRecCell(object):
     self.parent_rec_layer = None  # type: typing.Optional[RecLayer]
     self.parent_net = parent_net
     self.net_dict = safe_deep_copy(net_dict)
-    self.rec_layer_name = rec_layer_name
-    self.parent_get_layer = parent_get_layer
     from returnn.tf.network import TFNetwork, ExternData, LossHolder
     self.net = TFNetwork(
       name="%s/%s(rec-subnet)" % (parent_net.name, rec_layer_name),
